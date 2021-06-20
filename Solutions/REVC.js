4762985d24f128dc70f688f078064aaa68ebb39c
= <=>
2021-06-20 20:50:40 +1200

    solved but not super elegant

--- Solutions/REVC.js
+++ Solutions/REVC.js
@@ -6,8 +6,6 @@ const file = process.argv[2];
 
 //const testString = 'AAAACCCGGT';
 
-const revc_String =  fs.readFileSync(`../Data Sets/Problem Sets/${file}`, 'utf8');
-
 const complementValues = (x) => {
     switch(x){
         case 'A':
@@ -23,10 +21,15 @@ const complementValues = (x) => {
     }
 } 
 
-const resultString = [...revc_String].map(x => complementValues(x)).reverse().join('');
+if(file != null){
+    const revc_String =  fs.readFileSync(`../Data Sets/Problem Sets/${file}`, 'utf8');
 
-fs.writeFileSync('../Data Sets/Solution Sets/REVCSoln.txt', resultString);
+    const resultString = [...revc_String].map(x => complementValues(x)).reverse().join('');
+    
+    fs.writeFileSync('../Data Sets/Solution Sets/REVCSoln.txt', resultString);
+    
+    console.log(revc_String + '\n' + resultString);
+}
 
-console.log(revc_String + '\n' + resultString);
 
-
+module.exports = {complementValues};
